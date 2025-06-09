d812c2aae7e141442019d0410e7c9d20e1d0f332
Mit Patel <patel.m9521@gmail.com>
2025-06-09 17:03:46 +0100

    updated pipeline with model_evaluation_pipeline.py file

---
+++ src/data_science/pipeline/model_evaluation_pipeline.py
@@ -0,0 +1,26 @@
+from src.data_science.components.model_evaluation import ModelEvaluation
+from src.data_science import logger
+from src.data_science.config.configuration import ConfigurationManager
+
+STAGE_NAME = "Model Evaluation Stage"
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation.log_into_mlflow()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
