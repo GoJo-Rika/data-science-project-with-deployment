c5708afa2d7378f5175772e8f041ba7b95882112
Mit Patel <patel.m9521@gmail.com>
2025-06-09 17:08:16 +0100

    Updated main.py with Model Evaluation Stage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.data_science.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 from src.data_science.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 from src.data_science.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.data_science.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
@@ -44,6 +45,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.initiate_model_training()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelEvaluationTrainingPipeline()
+    model_trainer.initiate_model_evaluation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
