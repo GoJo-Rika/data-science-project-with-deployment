bd9cae7a4500a5ae33410187ae503bd14e3dc12f
Mit Patel <patel.m9521@gmail.com>
2025-06-09 13:45:35 +0100

    Updated main.py with Data Transformation Stage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.data_science.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.data_science.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.data_science import logger
 from src.data_science.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
@@ -20,6 +21,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
